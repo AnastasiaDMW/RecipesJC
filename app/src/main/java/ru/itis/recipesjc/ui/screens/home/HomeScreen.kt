e090a975fb18f37dd7dd2ed414f4052546c724de
AnastasiaDMW <AnastasiaZVV1@gmail.com>
2024-07-20 09:19:17 +0300

    add load and error screens

--- app/src/main/java/ru/itis/recipesjc/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/ru/itis/recipesjc/ui/screens/home/HomeScreen.kt
@@ -1,20 +1,31 @@
 package ru.itis.recipesjc.ui.screens.home
 
 import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -52,11 +63,11 @@ fun HomeBody(
     context: Context,
     modifier: Modifier
 ) {
-    LazyColumn {
-//        items() {
-//
-//        }
-    }
+//    LazyColumn {
+////        items() {
+////
+////        }
+//    }
 }
 
 @Composable
@@ -78,6 +89,36 @@ fun RecipeItem(context: Context, recipe: Recipe) {
             fontSize = 24.sp,
             color = Color.Black
         )
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.errorContainer,
+        trackColor = MaterialTheme.colorScheme.surface
+    )
+}
+
+@Composable
+fun ErrorScreen(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_connection_error),
+            contentDescription = "error image"
+        )
+        Text(
+            text = "Failed to load",
+            modifier = Modifier.padding(16.dp)
+        )
+        Button(onClick = retryAction) {
+            Text(text = "Retry")
+        }
     }
 }
 
